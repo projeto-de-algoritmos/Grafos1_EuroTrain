76cf048e6776e26b86b35435ee7e87c2c0b05b6a
Samuel Nogueira Bacelar <samuelnbacelar@hotmail.com>
2022-06-22 18:17:55 -0300

    criando botão

--- eurotrain/pages/index.tsx
+++ eurotrain/pages/index.tsx
@@ -11,43 +11,48 @@ import { Container,
 
 export default function Home() {
   return (
-    <Container>
-      <Card>
-        <CardBody>
-          <Form>
-            <FormGroup>
-                <Label for='startingPoint'>
-                  Selecione o ponto de partida
-                </Label>
-                <Input
-                  id="startingPoint"
-                  bsSize="lg"
-                  className="mb-3"
-                  type="select"
-                >
-                <option>
-                  Large Select
-                </option>
-              </Input>
-            </FormGroup>
-            <FormGroup>
-                <Label for='endingPoint'>
-                  Selecione o ponto de partida
-                </Label>
-                <Input
-                  id="endingPoint"
-                  bsSize="lg"
-                  className="mb-3"
-                  type="select"
-                >
-                <option>
-                  Large Select
-                </option>
-              </Input>
-            </FormGroup>
-          </Form>
-        </CardBody>
-      </Card>
+    <Container className="align-middle">
+      <Row style={{height: '100vh'}}className="align-items-center justify-content-center">
+        <Col lg={8}>
+          <Card>
+            <CardBody>
+              <Form>
+                <FormGroup>
+                    <Label for='startingPoint'>
+                      Selecione o ponto de partida
+                    </Label>
+                    <Input
+                      id="startingPoint"
+                      className="mb-3"
+                      type="select"
+                    >
+                    <option>
+                      Large Select
+                    </option>
+                  </Input>
+                </FormGroup>
+                <FormGroup>
+                    <Label for='endingPoint'>
+                      Selecione o ponto de partida
+                    </Label>
+                    <Input
+                      id="endingPoint"
+                      className="mb-3"
+                      type="select"
+                    >
+                    <option>
+                      Large Select
+                    </option>
+                  </Input>
+                </FormGroup>
+                <Row className="justify-content-end">
+                  <Col xs="auto"><Button color="primary">Procurar</Button></Col>
+                </Row>
+              </Form>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
     </Container>
 
   )
